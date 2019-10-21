83e0bb5ac97f890d0b51302bdc70ea5cabab65e0
Caitlin Coyiuto <coyiutoc@gmail.com>
2019-10-21 19:32:20 -0400

    Initial commit.

---
+++ src/components/LandingBlock/LandingBlock.js
@@ -0,0 +1,14 @@
+import React from "react";
+import "./LandingBlock.scss";
+
+const LandingBlock = () => {
+  return (
+    <div className = "landing-container grid-container">
+      <div>hi</div>
+      <div>Hello</div>
+    </div>
+
+  );
+};
+
+export default LandingBlock;
---
+++ src/components/Navbar/Navbar.js
@@ -0,0 +1,33 @@
+import React from "react";
+import Logo from "../../assets/Logo.png";
+import "./Navbar.scss";
+
+const Navbar = () => {
+  return (
+    <div className = "fixed-top">
+      <nav className="navbar navbar-custom top">
+        <div className="d-md-flex d-block flex-row mx-md-auto">
+            <a className="navbar-brand container" href="#">
+              <div className="title left block">DIMSUM</div>
+              <a className = "block" href="#">
+                <img width="50" src={Logo}></img>
+              </a>
+              <div className="title right block">DIARIES</div>
+            </a>
+        </div>
+      </nav>
+
+      <nav className="navbar navbar-custom bottom">
+        <div className="d-md-flex d-block flex-row mx-md-auto">
+          <div className = "container">
+            <a className="block link left" href="#">RESTAURANTS</a>
+            <a className="block link" href="#">ABOUT</a>
+            <a className="block link right" href="#">NEIGHBORHOODS</a>
+          </div>
+        </div>
+      </nav>
+    </div>
+  );
+};
+
+export default Navbar;
